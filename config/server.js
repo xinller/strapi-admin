24159ecd197517755afbc1e4ec7b7c0b207668d1
xinller <xl1012796466@gmail.com>
2021-01-06 23:34:01 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '69834d496808b62704d0282e93a79ad6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7c9d6400-51e6-4442-8fab-6f28f919c3e5'
+};
